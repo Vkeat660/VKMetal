504a225c2e0b11fade53c9564854d9e31cf5c209
Vkeat660 <Rainbow@Vivians-iMac.home>
2014-10-19 22:41:59 -0400

    Unblocked the main thread, added semaphores for future uniform read-write safety

--- VKMetal/VKMetal/MainViewController.swift
+++ VKMetal/VKMetal/MainViewController.swift
@@ -23,6 +23,8 @@ class MainViewController: UIViewController {
     var commandQueue : MTLCommandQueue?
     
     var displayLink : CADisplayLink?
+    var avaliableUniformBuffers: dispatch_semaphore_t?
+    let renderQueue = NSOperationQueue()
     
     let vertexData : [Float] = [ 0.0,  0.6, -4.0,
         -0.6, -0.6, -4.0,
@@ -47,6 +49,7 @@ class MainViewController: UIViewController {
         commandQueue = device.newCommandQueue()
         displayLink = CADisplayLink(target: self, selector: Selector("update"))
         displayLink?.addToRunLoop(NSRunLoop.mainRunLoop(), forMode: NSRunLoopCommonModes)
+        avaliableUniformBuffers = dispatch_semaphore_create(3)
         
     }
     
@@ -82,9 +85,9 @@ class MainViewController: UIViewController {
         let m43 = Float( 2.0 * (farZ * nearZ) / (nearZ - farZ))
         
         projectionData = [ xscale,    0.0,   0.0,  0.0,
-            0.0, yscale,   0.0,  0.0,
-            0.0,    0.0,   m33, -1.0,
-            0.0,    0.0,   m43,  0.0];
+                              0.0, yscale,   0.0,  0.0,
+                              0.0,    0.0,   m33, -1.0,
+                              0.0,    0.0,   m43,  0.0];
         
     }
     
@@ -108,7 +111,7 @@ class MainViewController: UIViewController {
         let pipelineStateDescriptor = MTLRenderPipelineDescriptor()
         pipelineStateDescriptor.vertexFunction = vertexProgram
         pipelineStateDescriptor.fragmentFunction = fragmentProgram
-        pipelineStateDescriptor.colorAttachments.objectAtIndexedSubscript(0).pixelFormat = .BGRA8Unorm
+        pipelineStateDescriptor.colorAttachments.objectAtIndexedSubscript(0).pixelFormat = MTLPixelFormat.BGRA8Unorm
         
         var pipelineError : NSError?
         pipelineState = device.newRenderPipelineStateWithDescriptor(pipelineStateDescriptor, error:&pipelineError)
@@ -119,28 +122,41 @@ class MainViewController: UIViewController {
     
     func render(){
         
-        let drawable = metalLayer.nextDrawable()
+        unowned let unownedSelf = self
+        renderQueue.addOperationWithBlock({ () -> Void in
+            let drawable = unownedSelf.metalLayer.nextDrawable()
+            
+            let renderPassDescriptor = MTLRenderPassDescriptor()
+            renderPassDescriptor.colorAttachments.objectAtIndexedSubscript(0).texture = drawable.texture
+            renderPassDescriptor.colorAttachments.objectAtIndexedSubscript(0).loadAction = MTLLoadAction.Clear
+            renderPassDescriptor.colorAttachments.objectAtIndexedSubscript(0).clearColor = MTLClearColor(red: 0.0,
+                green: 0.0,
+                blue: 0.0,
+                alpha: 1.0)
+            renderPassDescriptor.colorAttachments.objectAtIndexedSubscript(0).storeAction = MTLStoreAction.Store
+            
+            dispatch_semaphore_wait(unownedSelf.avaliableUniformBuffers, DISPATCH_TIME_FOREVER)
+            let commandBuffer = unownedSelf.commandQueue?.commandBuffer()
+            commandBuffer?.addCompletedHandler({ (MTLCommandBuffer) -> Void in
+                // Find a more elegant way to do this
+                var q = dispatch_semaphore_signal(unownedSelf.avaliableUniformBuffers)
+            })
+            
+            let commandEncoder = commandBuffer?.renderCommandEncoderWithDescriptor(renderPassDescriptor)
+            commandEncoder?.setRenderPipelineState(unownedSelf.pipelineState!)
+            commandEncoder?.setVertexBuffer(unownedSelf.vertexBuffer, offset: 0, atIndex: 0)
+            commandEncoder?.setVertexBuffer(unownedSelf.colorBuffer, offset: 0, atIndex: 1)
+            commandEncoder?.setVertexBuffer(unownedSelf.projectionBuffer, offset: 0, atIndex: 2)
+            commandEncoder?.drawPrimitives(MTLPrimitiveType.Triangle, vertexStart: 0, vertexCount: 3, instanceCount: 1)
+            commandEncoder?.endEncoding()
+            commandBuffer?.presentDrawable(drawable)
+            NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
         
-        let renderPassDescriptor = MTLRenderPassDescriptor()
-        renderPassDescriptor.colorAttachments.objectAtIndexedSubscript(0).texture = drawable.texture
-        renderPassDescriptor.colorAttachments.objectAtIndexedSubscript(0).loadAction = .Clear
-        renderPassDescriptor.colorAttachments.objectAtIndexedSubscript(0).clearColor = MTLClearColor(red: 0.0,
-            green: 0.0,
-            blue: 0.0,
-            alpha: 1.0)
-        renderPassDescriptor.colorAttachments.objectAtIndexedSubscript(0).storeAction = .Store
+                commandBuffer!.commit()
+            })
+                
+        })
         
-        let commandBuffer = commandQueue?.commandBuffer()
-        let renderEncoder = commandBuffer?.renderCommandEncoderWithDescriptor(renderPassDescriptor)
-        renderEncoder?.setRenderPipelineState(pipelineState!)
-        renderEncoder?.setVertexBuffer(vertexBuffer, offset: 0, atIndex: 0)
-        renderEncoder?.setVertexBuffer(colorBuffer, offset: 0, atIndex: 1)
-        renderEncoder?.setVertexBuffer(projectionBuffer, offset: 0, atIndex: 2)
-        renderEncoder?.drawPrimitives(.Triangle, vertexStart: 0, vertexCount: 3, instanceCount: 1)
-        renderEncoder?.endEncoding()
-        
-        commandBuffer?.presentDrawable(drawable)
-        commandBuffer?.commit()
         
     }
     
